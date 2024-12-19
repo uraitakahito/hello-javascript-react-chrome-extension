56d0c10479df67b24aea0b1a30b8cc2a5abe4d75
Takahito Urai <uraitakahito@gmail.com>
2024-12-19 23:34:22 +0900

    Enhance Vite configuration for custom build output structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        // https://rollupjs.org/configuration-options/#output-entryfilenames
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
   plugins: [react()],
 });
